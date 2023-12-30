6e70b0a995198347b546624dc6b1412234f1fd73
traczoskar <155188539+traczoskar@users.noreply.github.com>
2023-12-30 17:42:29 +0100

    added function organization to script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,53 @@
-let body = document.querySelector(".body");
-let header = document.querySelector(".header");
-let light = document.querySelector(".light");
-let buttonChangeBackground = document.querySelector(".js-footer__button");
-let hideShowTextButton = document.querySelector(".js-hideShowTextButton");
-let jsDiv = document.querySelector(".js-div");
-let hideShowPhotoButton = document.querySelector(".js-hideShowPhotoButton");
-let ironMaidenPhoto = document.querySelector(".js-section__photo");
+{
+    const showOrHideText = (hideShowTextButton) => {
 
-hideShowTextButton.addEventListener("click", () => {
-    jsDiv.classList.toggle("js-section__paragraph--display");
-    hideShowTextButton.innerText = jsDiv.classList.contains("js-section__paragraph--display")
-        ? "READ MORE"
-        : "HIDE TEXT";
-});
+        const jsDiv = document.querySelector(".js-div");
 
-hideShowPhotoButton.addEventListener("click", () => {
-    ironMaidenPhoto.classList.toggle("js-section__paragraph--display");
-    hideShowPhotoButton.innerText = ironMaidenPhoto.classList.contains("js-section__paragraph--display")
-        ? "SHOW PHOTO"
-        : "HIDE PHOTO";
-});
+        jsDiv.classList.toggle("js-section__paragraph--display");
+        hideShowTextButton.innerText = jsDiv.classList.contains("js-section__paragraph--display")
+            ? "READ MORE"
+            : "HIDE TEXT";
+    }
 
-buttonChangeBackground.addEventListener("click", () => {
-    body.classList.toggle("light");
-    header.classList.toggle("light");
-    buttonChangeBackground.innerText = body.classList.contains("light")
-        ? "DARK BACKGROUND"
-        : "LIGHT BACKGROUND";
-});
+    const showOrHidePhoto = (hideShowPhotoButton) => {
+
+        const ironMaidenPhoto = document.querySelector(".js-section__photo");
+
+        ironMaidenPhoto.classList.toggle("js-section__paragraph--display");
+        hideShowPhotoButton.innerText = ironMaidenPhoto.classList.contains("js-section__paragraph--display")
+            ? "SHOW PHOTO"
+            : "HIDE PHOTO";
+    }
+
+    const toggleBackground = (buttonChangeBackground) => {
+
+        const body = document.querySelector(".body");
+        const header = document.querySelector(".header");
+
+        body.classList.toggle("light");
+        header.classList.toggle("light");
+        buttonChangeBackground.innerText = body.classList.contains("light")
+            ? "DARK BACKGROUND"
+            : "LIGHT BACKGROUND";
+    }
+
+    const init = () => {
+
+        const hideShowTextButton = document.querySelector(".js-hideShowTextButton");
+        const hideShowPhotoButton = document.querySelector(".js-hideShowPhotoButton");
+        const buttonChangeBackground = document.querySelector(".js-footer__button");
+
+        hideShowTextButton.addEventListener("click", () => {
+            showOrHideText(hideShowTextButton)
+        });
+        hideShowPhotoButton.addEventListener("click", () => {
+            showOrHidePhoto(hideShowPhotoButton)
+        });
+        buttonChangeBackground.addEventListener("click", () => {
+            toggleBackground(buttonChangeBackground)
+        });
+    }
+
+    init();
+}
+
